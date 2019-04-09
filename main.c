47264810ae715082fd48903210916aa44ca856c2
max mccoy <maxmccoy01@gmail.com>
2019-04-09 01:34:52 +0000

    refined rotation encryption

--- main.c
+++ main.c
@@ -1,31 +1,26 @@
 #include <stdio.h>
 //the value of A in ASCII is 65, B = 66, C = 67,...., Z = 90
 //The values of the letters change with upper and lower case 
-void RotEncrypt(char origtext[7], int key); //takes entered text and key value. 
+void RotEncrypt(char origtext[100], int key); //takes entered text and key value. 
 
 int main(){
     int key = 1;
-    char origtext[7];//string with 51 characters
+    char origtext[100];//string capable of storing 99 letter words
     printf("please enter the text you want to encrypt in CAPITALS: ");
-    scanf("%s", &origtext); //stores the entered value in origtext
+    scanf("%s", origtext); //stores the entered value in origtext
     RotEncrypt(origtext,key);
-    //printf("the encrypted text is:%c\n ", Rotencrypt(origtext, key));
-} 
-
-void RotEncrypt(char origtext[7], int key){
+}
+void RotEncrypt(char origtext[100], int key){
     int i = 0; //counts
     int lettervalue; //variable that represents the number value of each ASCII charcter after the key has been applied
-    //char encryption;
-    int temp;
-    char encrypted[51];
+    int temp; // declared a temporary variable
+    char encrypted[100];
     
     while(origtext[i]!= '\0'){
-        temp  = origtext[i];
-        lettervalue = (temp + key - 65)%26+65;
-        encrypted[i] = lettervalue;
-        //lettervalue = ((int)origtext[i]- 65 + key) % 26 + 65; //65 is used as that is the value of A
-        //encryption = (char)lettervalue;
-        printf("%c", encrypted[i]);
+        temp  = origtext[i]; //takes the numerical value of the letter at i and stores it into temp
+        lettervalue = (temp + key - 65)%26+65; //conversion equation
+        encrypted[i] = lettervalue; //takes the numerical value of the letter after the equation and stores it at point i in the string
+        printf("%c", encrypted[i]); 
         i++;
     }
     printf("\n");
