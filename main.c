54cce42eb0798603ac417c17b3d74a63c6e2759d
max mccoy <maxmccoy01@gmail.com>
2019-04-20 09:54:27 +0000

    commented

--- main.c
+++ main.c
@@ -7,52 +7,58 @@ void RotDecrypt(char encryptext[100], int key2); //takes an encrypted text and t
 void SubEncrypt(char initialtext[100], char key3[27]); //takes entered text and key
 void SubDecrypt(char subencryptext[100], char key4[27]); //takes an encrypted text and the key that was used to encrypt it
 
+/* 
+Users will choose between rotation cipher encryption/decryption and substitution cipher
+encryption/decryption through a switch statement menu system. The user chooses between these 
+options by either pressing A, B, C or D. The user enters text and a key for each function.
+ */
+
 int main(){
-    char x;
+    char i;
     printf("Press:\n A for rotation cipher encryption\n B for rotation cipher decryption\n C for substitution cipher encryption\n D for substitution cipher decryption\n");
-    scanf("%c", &x);
-    switch(x) {
-    case 'A': ; int key1;
-              printf("please enter a key:");
-              scanf("%d", &key1); //user enters a value and it is stored in key1
-              int w = 0;
+    scanf("%c", &i);
+    switch(i) {
+    case 'A': ; int w = 0; //counts
               char c;
               char origtext[100];//string capable of storing 99 letter words
               printf("please enter the text you want to encrypt in CAPITALS:\n");
-              while(1){
+              while(1){ //a lopp that will allow whitespace to be used as well as characters
               scanf("%c", &c);
                 if(c == '\n'){
-                   origtext[w] = '\0';
+                   origtext[w] = '\0'; //when a new line the string ends
                    break;
                 }
-              origtext[w] = c;
+              origtext[w] = c; //that point in the string becomes that character
               w++;
-         }
+         }    
+              int key1;
+              printf("please enter a key:");
+              scanf("%d", &key1); //user enters a value and it is stored in key1
               RotEncrypt(origtext, key1); //calling the function to encrypt the text
               break;
               
-    case 'B': ; int key2;
-              printf("please enter the key used to encrypt the message:\n");
-              scanf("%d", &key2); //user gives the key they used to encrypt the message
-              char encryptext[100]; //the string that the encrypted text will be stored in
-              int x = 0;
+    case 'B': ; char encryptext[100]; //the string that the encrypted text will be stored in
+              int x = 0; //counts
               char a;
               printf("please enter the encrypted text in CAPITALS:\n");
               while(1){
               scanf("%c", &a);
-                if(d == '\n'){
+                if(a == '\n'){
                    encryptext[x] = '\0';
                    break;
                 }
               encryptext[x] = a;
               x++;
           }
+              int key2;
+              printf("please enter the key used to encrypt the message:\n");
+              scanf("%d", &key2); //user gives the key they used to encrypt the message
               RotDecrypt(encryptext, key2); //calling the function to decrypt the text
               break;
               
     case 'C': ; printf("please enter the message you want to encrypt in CAPITALS:\n");
               char initialtext[100];
-              int y = 0;
+              int y = 0; //counts
               char e;
               while(1){
               scanf("%c", &e);
@@ -69,10 +75,7 @@ int main(){
               SubEncrypt(initialtext, key3); //calling the function to encrypt the text
               break;
               
-    case 'D': ; printf("please enter the key used to encrypt the message in CAPITALS\n");
-              char key4[27];
-              scanf("%s", key4); //stores the key in key4
-              printf("please enter the encrypted message in CAPITALS:\n");
+    case 'D': ; printf("please enter the encrypted message in CAPITALS:\n");
               char subencryptext[100];
               int z = 0; // counts
               char u;
@@ -84,12 +87,21 @@ int main(){
                 }
               subencryptext[z] = u;
               z++;
-          }
+          }    
+              printf("please enter the key used to encrypt the message in CAPITALS\n");
+              char key4[27];
+              scanf("%s", key4); //stores the key in key4
               SubDecrypt(subencryptext, key4); //calling the function to decrypt the text
               break;
                 
     }
 }
+/*
+  This function accepts text and a key as an argument. The text is encrypted by a rotation
+  cipher. The size of the rotation is dependent on the size of the key. There is no return value,
+  instead the function prints the encrypted text. The entered text can be a maximum of 99 letters
+  long.
+ */
 void RotEncrypt(char origtext[100], int key1){
     int i = 0; //counts
     int lettervalue; //variable that represents the number value of each ASCII charcter after the key has been applied
@@ -111,7 +123,11 @@ void RotEncrypt(char origtext[100], int key1){
     }
     printf("\n");
 }
-
+/*
+  This function accepts a string of rotationally encrypted text and the key used to encrypt it as
+  arguments. The function has no return value, instead the function prints the decrypted text.
+  The encrypted text can be a maximum of 99 letters.
+ */
 void RotDecrypt(char encryptext[100], int key2){
     int i = 0; //counts
     int temp; //temporary variable
@@ -133,7 +149,12 @@ void RotDecrypt(char encryptext[100], int key2){
     }
     printf("\n");
 }
-
+/*
+  This function accepts text and a key as an argument. The text is encrypted by a substitution
+  cipher. The letter substitution is dependent on the key entered by the user. There is no return value,
+  instead the function prints the encrypted text. The entered text can be a maximum of 99 letters
+  long.
+*/
 void SubEncrypt(char initialtext[100], char key3[27]) {
    char subencrypted[100]; // the text after it has been encrypted
    int length = strlen(initialtext); //uses a function from string.h to find the lenth of initialtext
@@ -149,7 +170,11 @@ void SubEncrypt(char initialtext[100], char key3[27]) {
    }
    printf("\n");
 }
-
+/*
+  This function accepts a string of substitution encrypted text and the key used to encrypt it as
+  arguments. The function has no return value, instead the function prints the decrypted text.
+  The encrypted text can be a maximum of 99 letters.
+ */
 void SubDecrypt(char subencryptext[100], char key4[27]){
     char decryptedtext[100]; //string the decrypted text will be stored in
     int length = strlen(subencryptext); //stores the length of the string in the variable length
